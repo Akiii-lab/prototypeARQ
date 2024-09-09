3f4716b49aa06521fd52d7cf5c2598738a6e13e6
Akiii-lab <garciasierrandres@gmail.com>
2024-09-09 01:04:44 -0500

    first commit

---
+++ src/main/java/web/Prototipo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package web;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Prototipo {
+
+    public static void main(String[] args) {
+
+    }
+}
